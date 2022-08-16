fbaeabaa106f61c708bdcbca5e5b29357de43ac2
Andy Chumak <andrii.chumak@gooddata.com>
2022-08-16 11:19:47 +0200

    Converted the repo to use Python SDK instead of raw HTTP API

---
+++ scripts/pull.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# (C) 2022 GoodData Corporation
+import os, shutil, tempfile
+from gooddata_sdk import GoodDataSdk
+from pathlib import Path
+from dotenv import load_dotenv
+
+# TODO "gooddata_layouts" should be imported from the lib
+LAYOUT_ROOT_FOLDER = "gooddata_layouts"
+
+def main():
+    # Get current environment, defaulting to "development"
+    environment = os.getenv("GD_ENV", "development")
+
+    # Load corresponding env variables from file, if exists
+    load_dotenv(f".env.{environment}")
+
+    # Get all the necessary arguments from environment
+    host = os.getenv("GD_HOST")
+    if host is None:
+        raise RuntimeError("GD_HOST environmental variable must be defined")
+
+    token = os.getenv("GD_TOKEN")
+    if token is None:
+        raise RuntimeError("GD_TOKEN environmental variable must be defined")
+
+    layout_path = Path.cwd() / LAYOUT_ROOT_FOLDER
+
+    sdk = GoodDataSdk.create(host, token)
+
+    # Load new MD to a temp path. This way, if something goes wrong we will
+    # still have at least old MD. Also, drop the old MD before copying so that
+    # MD deleted on server is also deleted from declarative layout
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_path = Path(temp_dir)
+
+        # Pull user groups
+        sdk.catalog_user.store_declarative_user_groups(temp_path)
+
+        # Pull data sources and PDM
+        sdk.catalog_data_source.store_declarative_data_sources(temp_path)
+
+        # Pull workspaces
+        sdk.catalog_workspace.store_declarative_workspaces(temp_path)
+
+        # It seems the pull went OK, we can now drop original folder
+        if layout_path.exists():
+            shutil.rmtree(layout_path)
+
+        # and copy staff from temp folder
+        shutil.copytree(temp_path / LAYOUT_ROOT_FOLDER, layout_path)
+
+    print(f"Pulled definitions to {layout_path}")
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/push.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# (C) 2022 GoodData Corporation
+import os
+from gooddata_sdk import GoodDataSdk
+from pathlib import Path
+from dotenv import load_dotenv
+
+def main():
+    # Get current environment, defaulting to "development"
+    environment = os.getenv("GD_ENV", "development")
+
+    # Load corresponding env variables from file, if exists
+    load_dotenv(f".env.{environment}")
+
+    # Get all the necessary arguments from environment
+    host = os.getenv("GD_HOST")
+    if host is None:
+        raise RuntimeError("GD_HOST environmental variable must be defined")
+
+    token = os.getenv("GD_TOKEN")
+    if token is None:
+        raise RuntimeError("GD_TOKEN environmental variable must be defined")
+
+    credentials_path = os.getenv("GD_CREDENTIALS", f"credentials.{environment}.yaml")
+    if not Path(credentials_path).exists():
+        raise RuntimeError(f"Credentials file does not exist, trying to load from {credentials_path}")
+
+    root_path = Path.cwd()
+
+    sdk = GoodDataSdk.create(host, token)
+
+    # Push user groups
+    sdk.catalog_user.load_and_put_declarative_user_groups(root_path)
+
+    # Push data sources and PDM
+    sdk.catalog_data_source.load_and_put_declarative_data_sources(root_path, credentials_path)
+
+    # Push workspaces
+    sdk.catalog_workspace.load_and_put_declarative_workspaces(root_path)
+
+    print(f"Pushed definitions to {host}")
+
+if __name__ == "__main__":
+    main()
